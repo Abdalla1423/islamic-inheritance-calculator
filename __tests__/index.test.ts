c9a9f7fb59ceb258b7012a4e723dd6119fa0f265
Abdalla <a.eltayeb@campus.tu-berlin.de>
2024-08-16 19:59:47 +0200

    feat: add missing test cases

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1103,5 +1103,100 @@ describe("ALL TESTS", () => {
     checkResult(result, 'paternal_grand_father', f(8,27))
     checkResult(result, 'full_sister', f(4,27))
   })
+
+  //Fehlende Test Cases
+
+  //40
+  test('wife, 1 daughter, mother, 1 full_uncle', () => {
+    const result = calculate({
+      wife: 1,
+      daughter: 1,
+      mother: 1,
+      full_paternal_uncle: 1,
+    })
+    checkResult(result, 'wife', f(1,8))
+    checkResult(result, 'daughter', f(1,2))
+    checkResult(result, 'mother', f(1,6))
+    checkResult(result, 'full_paternal_uncle', f(5,24))
+  })
+
+  //57
+  test('wife, 1 son, mother', () => {
+    const result = calculate({
+      wife: 1,
+      son: 1,
+      mother: 1,
+    })
+    checkResult(result, 'wife', f(1,8))
+    checkResult(result, 'son', f(17,24))
+    checkResult(result, 'mother', f(1,6))
+  })
+
+  //60 --> falsch
+  test('wife, 1 daughter, father, mother, 1 full_brother', () => {
+    const result = calculate({
+      wife: 1,
+      daughter: 1,
+      father: 1,
+      mother: 1,
+      full_brother: 1,
+    })
+    checkResult(result, 'wife', f(1,8))
+    checkResult(result, 'daughter', f(1,2))
+    checkResult(result, 'father', f(5,24))
+    checkResult(result, 'mother', f(1,6))
+  })
+
+  //121
+  test('wife, 2 daughter, mother', () => {
+    const result = calculate({
+      wife: 1,
+      daughter: 2,
+      mother: 1,
+    })
+    checkResult(result, 'wife', f(1,8))
+    checkResult(result, 'daughter', f(7,10))
+    checkResult(result, 'mother', f(7,40))
+  })
+
+  //122
+  test('husband, mother, 1 full_sister, 1 paternal_sister, 1 maternal_sister', () => {
+    const result = calculate({
+      husband: 1,
+      mother: 1,
+      full_sister: 1,
+      paternal_sister: 1,
+      maternal_sibling: 1,
+    })
+    checkResult(result, 'husband', f(1,3))
+    checkResult(result, 'mother', f(1,9))
+    checkResult(result, 'full_sister', f(1,3))
+    checkResult(result, 'paternal_sister', f(1,9))
+    checkResult(result, 'maternal_sibling', f(1,9))
+  })
+
+  //128
+  test('husband, 1 full_sister, 1 paternal_sister', () => {
+    const result = calculate({
+      husband: 1,
+      full_sister: 1,
+      paternal_sister: 1,
+    })
+    checkResult(result, 'husband', f(3,7))
+    checkResult(result, 'full_sister', f(3,7))
+    checkResult(result, 'paternal_sister', f(1,7))
+  })
+
+  //129
+  test('husband, 1 full_sister, 1 paternal_brother, 1 paternal_sister', () => {
+    const result = calculate({
+      husband: 1,
+      full_sister: 1,
+      paternal_brother: 1,
+      paternal_sister: 1,
+    })
+    checkResult(result, 'husband', f(1,2))
+    checkResult(result, 'full_sister', f(1,2))
+  })
 })
 
